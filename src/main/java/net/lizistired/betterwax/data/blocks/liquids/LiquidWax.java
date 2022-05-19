86f46bbc8edae84d1ad86eb5d10f41815bf0a2a7
Liz Graham <rileygraham739@gmail.com>
2022-05-19 21:11:20 +0100

    Renamed ExampleLiquid to BaseFlowableFluid

--- src/main/java/net/lizistired/betterwax/data/blocks/liquids/LiquidWax.java
+++ src/main/java/net/lizistired/betterwax/data/blocks/liquids/LiquidWax.java
@@ -1,8 +1,6 @@
 package net.lizistired.betterwax.data.blocks.liquids;
 
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
-import net.minecraft.block.entity.BlockEntity;
 import net.minecraft.fluid.Fluid;
 import net.minecraft.fluid.FluidState;
 import net.minecraft.item.Item;
@@ -10,18 +8,12 @@ import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.state.StateManager;
 import net.minecraft.state.property.Properties;
-import net.minecraft.tag.FluidTags;
-import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.Direction;
-import net.minecraft.world.BlockView;
-import net.minecraft.world.WorldAccess;
-import net.minecraft.world.WorldView;
 
 import java.util.Optional;
 
 import static net.lizistired.betterwax.utils.RegHelper.*;
 
-public abstract class LiquidWax extends ExampleLiquid {
+public abstract class LiquidWax extends BaseFlowableFluid {
     @Override
     public Fluid getFlowing() {
         return FLOWING_LIQUIDWAX;
@@ -37,53 +29,9 @@ public abstract class LiquidWax extends ExampleLiquid {
         return WAX_BUCKET;
     }
 
-
-
-    @Override
-    protected boolean isInfinite() {
-        return true;
-    }
-
-    @Override
-    protected void beforeBreakingBlock(WorldAccess world, BlockPos pos, BlockState state) {
-        BlockEntity blockEntity = state.hasBlockEntity() ? world.getBlockEntity(pos) : null;
-        Block.dropStacks(state, world, pos, blockEntity);
-    }
-
-    @Override
-    public int getFlowSpeed(WorldView world) {
-        return 4;
-    }
-
     @Override
     protected BlockState toBlockState(FluidState fluidState) {
         return LIQUID_WAX.getDefaultState().with(Properties.LEVEL_15, getBlockStateLevel(fluidState));
-    }
-
-
-    @Override
-    public boolean matchesType(Fluid fluid) {
-        return fluid == STILL_LIQUIDWAX || fluid == FLOWING_LIQUIDWAX;
-    }
-
-    @Override
-    public int getLevelDecreasePerBlock(WorldView world) {
-        return 1;
-    }
-
-    @Override
-    public int getTickRate(WorldView world) {
-        return 5;
-    }
-
-    @Override
-    public boolean canBeReplacedWith(FluidState state, BlockView world, BlockPos pos, Fluid fluid, Direction direction) {
-        return direction == Direction.DOWN && !fluid.isIn(FluidTags.WATER);
-    }
-
-    @Override
-    protected float getBlastResistance() {
-        return 100.0f;
     }
 
     @Override
